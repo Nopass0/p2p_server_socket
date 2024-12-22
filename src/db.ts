f16760c4a476c903ec1d2ba025c0a5b010cfa9f7
Nopass0 <gal.bogdan2015@gmail.com>
2024-12-22 18:09:19 +0500

    upd

--- src/db.ts
+++ src/db.ts
@@ -1,23 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient().$extends({
-    model: {
-      // You can add model extensions here if needed
-    },
-  });
-};
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-};
-
-const db = globalForPrisma.prisma ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = db;
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
-export default db;
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
+
+export default prisma;
